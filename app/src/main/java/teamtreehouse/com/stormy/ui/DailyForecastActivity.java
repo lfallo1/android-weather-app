1b03925c368722dce4834b63fe615a4a34f8cfcb
Lance Fallon <lancefallon@Lances-MBP.fios-router.home>
2016-08-16 18:52:19 -0400

    initial commit

---
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,39 @@
+package teamtreehouse.com.stormy.ui;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+
+import java.util.Arrays;
+
+import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.adapters.DayAdapter;
+import teamtreehouse.com.stormy.weather.Day;
+
+public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
+    }
+}
+
+
+
+
+
+
+
+
+
